5562f051193fd60764a83ef28f5aca6f6ee4d1cc
ricardomapurunga <164086194+ricardomapurunga@users.noreply.github.com>
2024-03-20 13:18:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
-var frameNumber = 0, // start video at frame 0
-    // lower numbers = faster playback
-    playbackConst = 500, 
-    // get page height from video duration
-    setHeight = document.getElementById("set-height"), 
-    // select video element         
-    vid = document.getElementById('v0'); 
+const html = document.documentElement;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
 
-// dynamically set the page height according to video length
-vid.addEventListener('loadedmetadata', function() {
-  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+const frameCount = 367;
+const currentFrame = (index) =>
+  `gif2/frame_${index.toString().padStart(5, "0")}.gif`;
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image();
+img.src = currentFrame(1);
+
+const drawImage = () => {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const aspectRatio = img.width / img.height;
+  const canvasAspectRatio = windowWidth / windowHeight;
+  let imgWidth, imgHeight, offsetX, offsetY;
+
+  if (canvasAspectRatio > aspectRatio) {
+    imgWidth = windowWidth;
+    imgHeight = imgWidth / aspectRatio;
+    offsetX = 0;
+    offsetY = (windowHeight - imgHeight) / 2;
+  } else {
+    imgHeight = windowHeight;
+    imgWidth = imgHeight * aspectRatio;
+    offsetY = 0;
+    offsetX = (windowWidth - imgWidth) / 2;
+  }
+
+  canvas.width = windowWidth;
+  canvas.height = windowHeight;
+  context.drawImage(img, offsetX, offsetY, imgWidth, imgHeight);
+};
+
+img.onload = drawImage;
+
+window.addEventListener("resize", drawImage);
+
+const updateImage = (index) => {
+  img.src = currentFrame(index);
+  //drawImage();
+};
+
+window.addEventListener("scroll", () => {
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+
+  requestAnimationFrame(() => updateImage(frameIndex + 1));
 });
 
-// Use requestAnimationFrame for smooth playback
-function scrollPlay(){  
-  var frameNumber  = window.pageYOffset/playbackConst;
-  vid.currentTime  = frameNumber;
-  window.requestAnimationFrame(scrollPlay);
-}
-
-window.requestAnimationFrame(scrollPlay);
+preloadImages();
